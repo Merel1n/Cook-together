0ed4ab7b1d4bee1483255da32accc0fcc0d0e311
Inna Matviienko <ineskamit@gmail.com>
2025-01-13 19:05:57 +0100

    settings

---
+++ src/components/App.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <heder>
+      <div className='menu'>
+      <a href='/' className='logo'>CookTogether</a>
+      <div className='menuList'>
+      <a href='/sigin'>SigIn</a>
+      <a href='/login'>LogIn</a>
+      <a href='/aboutus'>AboutUs</a>
+      <a href='/contact'>Contact</a>
+      </div>
+      
+      </div>
+      
+
+    </heder>
+    <body>
+    <h1>Cook together</h1>  
+    </body>
+    <footer></footer>
+
+    </>
+  )
+}
+
+export default App
